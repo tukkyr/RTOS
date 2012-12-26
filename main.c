8a2572d06ae484acd38462d9127a3085fae893a2
tukkyr <tukky.apn@gmail.com>
2012-12-27 02:18:41 +0900

    RTOS3nd

--- main.c
+++ main.c
@@ -2,12 +2,38 @@
 #include "serial.h"
 #include "lib.h"
 
+int global_data = 0x10;
+int global_bss;
+static int static_data = 0x20;
+static int static_bss;
+
+static int init(void)
+{
+	extern int erodata, data_start, edata, bss_start, ebss;
+	memcpy(&data_start, &erodata, (long)&edata - (long)&data_start);
+	memset(&bss_start, 0, (long)&ebss - (long)&bss_start);
+	serial_init(SERIAL_DEFAULT_DEVICE);
+}
+
+static void printval(void)
+{
+	puts("global_data = "); putxval(global_data, 0); puts("\n");
+	puts("global_bss = "); putxval(global_bss, 0); puts("\n");
+	puts("static_data = "); putxval(static_data, 0); puts("\n");
+	puts("static_bss = "); putxval(static_bss, 0); puts("\n");
+}
+
 int main(void)
 {
-	serial_init(SERIAL_DEFAULT_DEVICE);
+	init();
 	puts("Hello World!\n");
-	putxval(0x10, 0); putc('\n');
-	putxval(0xffff, 0); putc('\n');
+	printval();
+	puts("overwrite variables.\n");
+	global_data = 0x20;
+	global_bss = 0x30;
+	static_data = 0x40;
+	static_bss = 0x50;
+	printval();
 	while (1);
 
 	return 0;
