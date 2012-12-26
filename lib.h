e4b85142528b1c542bc33493d9b29d9f455c012f
tukkyr <tukky.apn@gmail.com>
2012-12-27 00:57:57 +0900

    RTOS2nd

--- lib.h
+++ lib.h
@@ -3,5 +3,13 @@
 
 int putc(unsigned char c);
 int puts(unsigned char* str);
+void *memset(void *b, int c, long len);
+void *memcpy(void *dst, const void *src, long len);
+int memcmp(const void *b1, const void *b2, long len);
+int strlen(const char *s);
+char *strcpy(char *dst, const char *src);
+int strcmp(const char *s1, const char *s2);
+int strncmp(const char *s1, const char *s2, int len);
+int putxval(unsigned long value, int column);
 
 #endif /* _LIB_H_ */
--- main.c
+++ main.c
@@ -6,7 +6,8 @@ int main(void)
 {
 	serial_init(SERIAL_DEFAULT_DEVICE);
 	puts("Hello World!\n");
-
+	putxval(0x10, 0); putc('\n');
+	putxval(0xffff, 0); putc('\n');
 	while (1);
 
 	return 0;
